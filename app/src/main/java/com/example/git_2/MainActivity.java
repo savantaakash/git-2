4ce325651de989751bf402812965f1afba76b78e
Savant Aakash <savantaakash322@gmail.com>
2020-07-18 15:09:20 +0530

    modified xml handle to MainActivity.java

--- app/src/main/java/com/example/git_2/MainActivity.java
+++ app/src/main/java/com/example/git_2/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.git_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+TextView textView;
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview);
+        button = findViewById(R.id.button);
     }
 }
